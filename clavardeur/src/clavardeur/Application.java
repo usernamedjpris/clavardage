bdbbf3b4c48de1d2b337393278e09e714e250cae
RemiF <fache@etud.insa-toulouse.fr>
2019-12-05 12:18:32 +0100

    Update updated, choice of conception, work on network on jeremy pc

--- clavardeur/src/clavardeur/Application.java
+++ clavardeur/src/clavardeur/Application.java
@@ -1,5 +1,6 @@
 package clavardeur;
 
+import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -8,8 +9,9 @@ import java.util.Observer;
 
 public class Application implements Observer {
 	Utilisateur user;
-	HashMap<String,String> conv;
+	HashMap<String,ConversationGui> conv;
 	VuePrincipale main;
+	static BD maBD=BD.getBD();
 	public static void main(String[] args) {
 		
 		Application monApp=new Application();
@@ -18,7 +20,7 @@ public class Application implements Observer {
 	Application(){
 		Enumeration<NetworkInterface> net= NetworkInterface.getNetworkInterfaces();//to work offline
 		byte[] mac=net.nextElement().getHardwareAddress();
-		user= new Utilisateur((mac.toString()).hashCode()); //fixe par poste (adresse mac by eg)
+		user= new Utilisateur((mac.toString()).hashCode(),InetAddress.getLocalHost()); //fixe par poste (adresse mac by eg)
 		conv=new HashMap<String,ConversationGui>;
 		main=new VuePrincipale(this);
 		main.show();
@@ -32,23 +34,40 @@ public class Application implements Observer {
 	//before check unicty pseudo
 	static void sendPseudoSwitch(String old, String newPseudo,long  id) {
 		Reseau.getReseau().sendDataBroadcast("SWITCH".getBytes()+" "+old.getBytes()+" "+newPseudo.getBytes());
-		BD maBd= BD.getBD();
 		maBD.delIdPseudoLink(old);
 		maBD.setIdPseudoLink(newPseudo,id);
 	}
-	boolean checkUnicity(String pseudo) {
-		return BD.getBD().checkUnicity();
+	static boolean checkUnicity(String pseudo) {
+		return maBD.checkUnicity();
 	}
 	void deconnexion(String pseudo) {
-		Reseau.getReseau().sendDataBroadcast("DECONNEXION".getBytes()+" "+pseudo.getBytes());
+		Reseau.getReseau().sendDataBroadcast(new Message(message.Type.DECONNECTION,user.getPersonne());
 	}
-	public createConversation(toPseudo) {
-		conv.
+	public createConversation(Personne toPersonne) {
+		ArrayList<Message> hist=maBD.getHistorique(maBD.getIdPersonne(toPersonne.getPseudo()));
+		conv.put(toPersonne.getPseudo(), new ConversationGui(new Conversation(toPersonne,hist ),10));
 	}
 	@Override
 	public void update(Observable o, Object arg) {
 		//try convert arg to message 
 		//if message => convGUI update
+		  if (arg instanceof Message) {  
+	           Message message = (Message) arg;  
+	           if(message.getType()==message.Type.DEFAULT)
+	        	   conv.get(message.getEmetteur()).update(message);
+	           else if(message.getType()==message.Type.SWITCH) {
+	        	   long id =maBD.getIdPersonne(message.getEmetteur);
+	        	   maBD.delIdPseudoLink(message.getEmetteur);
+	       		   maBD.setIdPseudoLink(message.getNewPseudo(),id);
+	           }
+	           else if(message.getType()==message.Type.DECONNECTION) {
+	        	   conv.get(message.getEmetteur()).deconnection();
+	           }
+	           else if(message.getType()==message.Type.DECONNECTION) {
+	        	   conv.get(message.getEmetteur()).deconnection();
+	           }
+	        	   
+	        }  
 		//try string
 		//si string and swithc
 		//update pseudo
--- clavardeur/src/clavardeur/Utilisateur.java
+++ clavardeur/src/clavardeur/Utilisateur.java
@@ -1,19 +1,21 @@
 package clavardeur;
 
+import java.net.InetAddress;
+
 public class Utilisateur {
-	private String pseudo;
+	private Personne who;
 	private long idUtilisateur;
 	
-	public Utilisateur(long idU) {
+	public Utilisateur(long idU, InetAddress ip) {
 		this.idUtilisateur = idU;
-		this.pseudo = "anonymous";
+		who=new Personne(ip,"anonymous" );
 	}
 	
 	public Boolean setPseudo (String nouveauPseudo) {
 		Boolean bool = true;
-		if Application.checkUnicity(nouveauPseudo) {			
-			Application.sendPseudoSwitch(this.pseudo, nouveauPseudo, this.idUtilisateur)
-			this.pseudo = nouveauPseudo;
+		if (Application.checkUnicity(nouveauPseudo) {			
+			Application.sendPseudoSwitch(who.getPseudo(), nouveauPseudo, this.idUtilisateur)
+			who.setPseudo(nouveauPseudo);
 		} else {
 			bool = false;
 		}
