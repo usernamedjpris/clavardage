6245e5802e1455a185e30e6b6af63510d76a4731
usernamedjpris <49071112+usernamedjpris@users.noreply.github.com>
2019-12-16 16:12:10 +0100

    reponses aux questions et VueChoixPseudo

--- src/VueChoixPseudo.java
+++ src/VueChoixPseudo.java
@@ -2,13 +2,23 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GridLayout;
 
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+
 import javax.swing.JLabel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.SwingConstants;
 import javax.swing.JSplitPane;
 import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.Color;
 
-public class VueChoixPseudo {
+
+public class VueChoixPseudo  implements ActionListener{
 
 	private JFrame frame;
 	private JTextField textField;
@@ -41,60 +51,84 @@ public class VueChoixPseudo {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 710, 467);
+		frame.setBounds(100, 100, 683, 321);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new GridLayout());
+		frame.getContentPane().setLayout(new GridLayout(6, 3));
 		
-		JLabel label = new JLabel("");
-		frame.getContentPane().add(label, "cell 0 0,grow");
+		JLabel lblvide1 = new JLabel("");
+		JLabel lblvide2 = new JLabel("");
+		JLabel lblvide3 = new JLabel("");
+		JLabel lblvide4 = new JLabel("");
+		JLabel lblvide5 = new JLabel("");
+		JLabel lblvide6 = new JLabel("");
+		JLabel lblvide7 = new JLabel("");
+		JLabel lblvide9 = new JLabel("");
+		JLabel lblvide10 = new JLabel("");
+		JLabel lblvide11 = new JLabel("");
+		JLabel lblvide12 = new JLabel("");
+		JLabel lblvide13 = new JLabel("");
+		JLabel lblvide14 = new JLabel("");
+
+
+		frame.getContentPane().add(lblvide1);
+		frame.getContentPane().add(lblvide2);		
+		frame.getContentPane().add(lblvide3);
+		frame.getContentPane().add(lblvide4);
+		JLabel lblPseudoDuJour = new JLabel("Pseudo du jour :");
+		lblPseudoDuJour.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPseudoDuJour.setFont(new Font("Courier New", Font.BOLD, 20));
+		frame.getContentPane().add(lblPseudoDuJour);
+		frame.getContentPane().add(lblvide5);
+		frame.getContentPane().add(lblvide6);
 		
-		JLabel label_1 = new JLabel("");
-		frame.getContentPane().add(label_1, "cell 1 0,grow");
-		
-		JLabel label_2 = new JLabel("");
-		frame.getContentPane().add(label_2, "cell 0 1,grow");
-		
-		JLabel lblPseudoDuJour = new JLabel("pseudo du jour");
-		lblPseudoDuJour.setFont(new Font("Courier New", Font.BOLD, 15));
-		frame.getContentPane().add(lblPseudoDuJour, "cell 1 1,grow");
-		
-		JLabel label_3 = new JLabel("");
-		frame.getContentPane().add(label_3, "cell 0 2,grow");
-		
-		JLabel label_4 = new JLabel("");
-		frame.getContentPane().add(label_4, "cell 1 2,grow");
-		
-		JLabel label_5 = new JLabel("");
-		frame.getContentPane().add(label_5, "cell 0 3,grow");
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1);
 		
 		JSplitPane splitPane = new JSplitPane();
-		frame.getContentPane().add(splitPane, "cell 1 3,grow");
+		panel_1.add(splitPane);
 		
-		textField = new JTextField();
-		splitPane.setRightComponent(textField);
-		textField.setColumns(10);
-		
-		JLabel lblPseudo = new JLabel("pseudo");
-		lblPseudo.setFont(new Font("Courier New", Font.PLAIN, 13));
+		JLabel lblPseudo = new JLabel("pseudo :");
+		lblPseudo.setBackground(new Color(240, 240, 240));
+		lblPseudo.setFont(new Font("Courier New", Font.PLAIN, 17));
 		splitPane.setLeftComponent(lblPseudo);
 		
-		JLabel label_6 = new JLabel("");
-		frame.getContentPane().add(label_6, "cell 0 4,grow");
+		textField = new JTextField();
+		textField.setFont(new Font("Courier New", Font.BOLD | Font.ITALIC, 13));
+		splitPane.setRightComponent(textField);
+		textField.setColumns(10);
+		frame.getContentPane().add(lblvide7);
+		frame.getContentPane().add(lblvide9);
+		frame.getContentPane().add(lblvide10);
+		frame.getContentPane().add(lblvide11);
+		frame.getContentPane().add(lblvide12);
 		
-		JLabel label_7 = new JLabel("");
-		frame.getContentPane().add(label_7, "cell 1 4,grow");
-		
-		JLabel label_8 = new JLabel("");
-		frame.getContentPane().add(label_8, "cell 0 5,grow");
-		
-		JLabel label_9 = new JLabel("");
-		frame.getContentPane().add(label_9, "cell 1 5,grow");
-		
-		JLabel label_10 = new JLabel("");
-		frame.getContentPane().add(label_10, "cell 0 6,grow");
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel);
 		
 		JButton btnLogin = new JButton("login");
-		frame.getContentPane().add(btnLogin, "cell 1 6,grow");
-	}
+		btnLogin.setBackground(new Color(102, 205, 170));
+		btnLogin.setForeground(Color.WHITE);
+		btnLogin.setFont(new Font("Calibri", Font.BOLD, 13));
+		btnLogin.setSize(20,50);
+		panel.add(btnLogin);
+		btnLogin.addActionListener(this);
+		frame.getContentPane().add(lblvide13);
 
+		
+	}
+	public void actionPerformed(ActionEvent ae)
+	 {
+	   String uname = textField.getText();
+	   //if(uname.checkunicity() && uname != "") //check unicity
+	   //{
+	   System.out.println("ok : "+uname);
+
+	    //}
+	    //else
+	    //{
+	    //  JOptionPane.showMessageDialog(this,"Incorrect login or password",
+	    //  "Error",JOptionPane.ERROR_MESSAGE);  
+	    //}
+	  //}
+	 }
 }
